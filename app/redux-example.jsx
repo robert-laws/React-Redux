815dc12c026a4c1cca739f925df3b18c5a2e7668
Robert Laws <roblaws@me.com>
2017-02-19 12:08:43 +0300

    redux example

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -5,10 +5,27 @@ console.log('Starting redux example');
 var reducer = (state = { name: 'Anonymous' }, action) => {
   // state = state || { name: 'Bob' }; // default argument
 
-  return state;
+  switch(action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      }
+    default:
+      return state;
+  }
 }
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Bob'
+}
+
+store.dispatch(action);
+
+console.log('Name should be Bob', store.getState());
