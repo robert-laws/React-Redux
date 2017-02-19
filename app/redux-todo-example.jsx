0582b6e8261627dda291c70a3b56c8df5394dc37
robert-laws <robaws@me.com>
2017-02-19 21:24:26 +0300

    add subscribe and dev tools

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -5,6 +5,7 @@ console.log('Starting redux example');
 var stateDefault = {
   searchText: '',
   showCompleted: false,
+  name: 'UNKNOWN',
   todos: []
 }
 
@@ -15,16 +16,42 @@ var reducer = (state = stateDefault, action) => {
         ...state,
         searchText: action.searchText
       }
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      }
     default:
       return state;
   }
 }
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// subscribe to changes
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+  // console.log('name is', state.name);
+  document.getElementById('reduxTest').innerHTML = state.name;
+});
+// unsubscribe(); // callback function to unsubscribe
 
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'work'
 });
 
-console.log('search test should be work', store.getState());
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Kal Kool'
+});
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Jim Jupe'
+});
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Bob Cobb'
+});
