e523924886a75c1d31984448d5214919745a4cf6
Robert Laws <roblaws@me.com>
2017-02-20 09:37:08 +0300

    Add actions for adding/removie state content

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,30 +2,106 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = { name: 'Anonymous' }, action) => {
-  // state = state || { name: 'Bob' }; // default argument
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+}
 
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
   switch(action.type) {
     case 'CHANGE_NAME':
       return {
         ...state,
         name: action.name
       }
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      }
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      }
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
+      }
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      }
     default:
       return state;
   }
 }
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-var currentState = store.getState();
-console.log('currentState', currentState);
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+});
 
-var action = {
+store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Bob'
-}
+});
 
-store.dispatch(action);
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
 
-console.log('Name should be Bob', store.getState());
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Reading'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Sports'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Matrix',
+  genre: 'Sci-Fi'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Office Space',
+  genre: 'Comedy'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+})
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
+})
+
+console.log('New State', store.getState());
